af0ba3850587de0472fefe7994580a44763d9078
IanCoolidge0 <ian.coolidge@asdnh.org>
2017-10-05 15:10:44 -0400

    added some more example code

--- src/org/usfirst/frc/team2342/robot/Robot.java
+++ src/org/usfirst/frc/team2342/robot/Robot.java
@@ -1,7 +1,31 @@
 package org.usfirst.frc.team2342.robot;
 
+import org.usfirst.frc.team2342.robot.actions.ActionGoDistance;
+import org.usfirst.frc.team2342.robot.actions.ActionQueue;
+
 import edu.wpi.first.wpilibj.SampleRobot;
 
 public class Robot extends SampleRobot {
 	
+	private ActionQueue queue;
+	
+	private void setupAuto()
+	{
+		
+		queue.appendAction(new ActionGoDistance(5));
+	}
+	
+	public Robot()
+	{
+		queue = new ActionQueue();
+	}
+	
+	@Override
+	public void autonomous()
+	{
+		setupAuto();
+		
+		while(isEnabled() && !queue.isCompleted())
+			queue.execute();
+	}	
 }
